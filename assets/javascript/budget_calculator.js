59bb28c50ed65353597d3bc45a73d218f7d96c83
Heltta <lucashelgueraz@gmail.com>
2022-05-19 22:54:45 -0300

    Added shipping cost functionality
    Some console.log function were left for debbuging purposes

--- assets/javascript/budget_calculator.js
+++ assets/javascript/budget_calculator.js
@@ -1,9 +1,6 @@
 const marcas = ["APPLE", "XIAOMI", "SAMSUNG", "LG", "DESCONOCIDA"];
 
-function datoMalIngresado(nombreInput){
-    alert( nombreInput + " no encontrado/a, vuelva a intentar");
-    return
-}
+
 
 class PHONE{
     constructor(model, brand="DESCONOCIDA", price=0){
@@ -55,6 +52,10 @@ phoneStock.push(new PHONE("nexus 7", "lg", 560));
 phoneStock.push(new PHONE("nexus 6", "lg", 250));
 
 function estimarPresupuesto(celulares, brands){
+    function datoMalIngresado(nombreInput){
+        alert( nombreInput + " no encontrado/a, vuelva a intentar");
+        return
+    }
     //Input de MARCA
     let marca = prompt("Ingrese la marca de su dispositivo").toUpperCase();
     while(!brands.includes(marca)){
@@ -85,6 +86,130 @@ function estimarPresupuesto(celulares, brands){
     alert("El presupuesto de su reparación es " + presupuesto + " dolares");
 }
 
+function costoEnvio(){
+    //Se asume que el cliente confirmó que quiere envio
+    const coveredDepartment = [
+        {name:"Montevideo",     ticketLevel: 0},
+        {name:"Canelones",      ticketLevel: 1},
+        {name:"Maldonado",      ticketLevel: 1},
+        {name:"Florida",        ticketLevel: 1},
+        {name:"Lavalleja",      ticketLevel: 1},
+        {name:"San José",       ticketLevel: 1},
+        {name:"Colonia",        ticketLevel: 2},
+        {name:"Cerro Largo",    ticketLevel: 2},
+        {name:"Soriano",        ticketLevel: 2},
+        {name:"Rocha",          ticketLevel: 2},
+        {name:"Durazno",        ticketLevel: 2},
+        {name:"Treinta y Tres", ticketLevel: 2},
+        {name:"Flores",         ticketLevel: 2},
+        {name:"Tacuarembó",     ticketLevel: 3},
+        {name:"Paysandú",       ticketLevel: 3},
+        {name:"Río Negro",      ticketLevel: 3},
+        {name:"Rivera",         ticketLevel: 3},
+        {name:"Salto",          ticketLevel: 3},
+        {name:"Artigas",        ticketLevel: 3}
+    ];
+    const coveredNeighborhood = [
+        {name:"Centro",         ticketLevel: 0},
+        {name:"Ciudad Vieja",   ticketLevel: 0},
+        {name:"Cordón",         ticketLevel: 0},
+        {name:"Punta carretas", ticketLevel: 0},
+        {name:"Prado",          ticketLevel: 1},
+        {name:"Tres cruces",    ticketLevel: 1},
+        {name:"Malvin",         ticketLevel: 1},
+        {name:"Manga",          ticketLevel: 2},
+        {name:"Carrasco",       ticketLevel: 2}
+    ];
+
+    function zonaNoEncontrada(nombreInput){
+        alert( nombreInput + " no esta dentro de nuestra zona de cobertura");
+        return
+    }
+    
+    function getLvL(lugar, zonaCubierta){
+        let zonaObj = zonaCubierta.find(zona => zona.name.toUpperCase() == lugar);
+        if(undefined == zonaObj){
+            return -1;
+        }
+        console.log("zonaObj es \n");
+        console.log(zonaObj);
+        console.log(zonaObj.ticketLevel);
+        console.log(typeof(zonaObj.ticketLevel));
+        return parseInt(zonaObj.ticketLevel);
+    }
+
+    function ticketCost(departmentLvL, nhoodLvL){
+        //Precondicion: Si el barrio no aplica, nhoodLvL es -1
+        const departmentCost = [0, 10, 30, 35]; //dolares
+        const nhoodCost = [0, 5, 8]; //dolares
+        console.log("nivel hood es \n");
+        console.log(nhoodLvL);
+        console.log("nivel depmt \n");
+        console.log(departmentLvL);
+        if (nhoodLvL>0){
+            console.log("costo hood es \n");
+            console.log(nhoodCost[nhoodLvL]);
+            return departmentCost[departmentLvL] + nhoodCost[nhoodLvL];
+        }
+        return departmentCost[departmentLvL];
+    }
+    
+    //Input de departamento
+    let departamento = prompt("Ingrese su departamento").toUpperCase();
+    let nivelInterdep = getLvL(departamento, coveredDepartment);
+    while(nivelInterdep == -1){
+        //Me quedo hasta que el usuario ingrese un input correcto
+        zonaNoEncontrada(departamento);
+        departamento = prompt("Ingrese su departamento").toUpperCase();
+        nivelInterdep = getLvL(departamento, coveredDepartment);
+    }
+
+    let nivelUrbano;
+    console.log("departamento es \n");
+    console.log(departamento);
+    if(departamento == coveredDepartment[0].name.toLocaleUpperCase()){
+        //0 es montevideo
+        //Input del barrio de montevideo
+        console.log("estoy en montevideo");
+        let barrio = prompt("Ingrese su barrio").toUpperCase();
+        nivelUrbano = getLvL(barrio, coveredNeighborhood);
+        console.log("en un barrio de nivel");
+        console.log(nivelUrbano);
+        console.log(typeof(nivelUrbano));
+        while(nivelUrbano == -1){
+            //Me quedo hasta que el usuario ingrese un input correcto
+            zonaNoEncontrada(barrio);
+            barrio = prompt("Ingrese su barrio").toUpperCase();
+            nivelUrbano = getLvL(barrio, coveredNeighborhood);
+        }
+    }
+    else{
+        console.log("estoy en el interior");
+        nivelUrbano = -1;
+    }
+
+    console.log("llamo a ticket con");
+    console.log(nivelInterdep);
+    console.log(nivelUrbano);
+    let costo = ticketCost(nivelInterdep,parseInt(nivelUrbano));
+    console.log("costo es \n");
+    console.log(costo);
+    if(isNaN(costo)){
+        alert("error, costo isNaN");
+    }
+    else if(costo < 0){
+        alert("error, costo menor a 0");
+    }
+    else{
+        alert("Su costo de envío es " + Math.round(costo) + " dolares\npor enviar a " + departamento);
+    }
+    return
+}
+
 while(prompt("Quiere estimar su presupuesto [si/no]").toUpperCase() == "SI"){
     estimarPresupuesto(phoneStock, marcas);
+}
+
+while(prompt("Quiere calcular el costo de envío [si/no]").toUpperCase() == "SI"){
+    costoEnvio();
 }
